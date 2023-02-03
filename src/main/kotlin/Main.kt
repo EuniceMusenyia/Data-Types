a6483f1acb027e01a49a78be45265048dcbe0034
student <eunicemusenyia@gmail.com>
2023-02-03 07:17:22 +0300

    created variables and data types

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var fullname = "Eunice Musenyia"
+    var age = "21"
+    var phoneNumber = "0710368387"
+    var weightInKgs = 56
+    val isKenyan = true
+    val notKenyan = false
+
+    println(age)
+
+
+}
